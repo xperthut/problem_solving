4ea92fdaf5aac0b80a313edb89bf2d18bea1bc21
Methun Kamruzzaman <mhmethun@gmail.com>
2022-07-17 21:45:28 -0400

    updated

--- Weird_Algorithm.py
+++ Weird_Algorithm.py
@@ -1,5 +1,7 @@
 """
-Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+Consider an algorithm that takes as input a positive integer n. 
+If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. 
+The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
 3→10→5→16→8→4→2→1
 
 Your task is to simulate the execution of the algorithm for a given value of n.
@@ -23,3 +25,22 @@ Output:
 3 10 5 16 8 4 2 1
 """
 
+from sys import setrecursionlimit
+setrecursionlimit(2000005)
+
+
+if __name__=="__main__":
+    n = int(input())
+    print(n, end=' ')
+    
+    while n>1:
+        if n%2:
+            n=3*n+1
+        else:
+            n=n//2
+            
+        if n==1:
+            print(n)
+        else:
+            print(n, end=' ')
+            
